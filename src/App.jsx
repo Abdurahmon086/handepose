c034e47e43b35ff5a615769fa8fc1eba7fff55fb
Turdaliyev Abdurahmon <121414053+Abdurahmon086@users.noreply.github.com>
2024-05-14 20:37:20 +0500

    fingerpose added

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,99 @@
-import { useEffect, useRef } from 'react'
-import * as tf from '@tensorflow/tfjs'
-import * as handpose from '@tensorflow-models/handpose'
-import Webcam from 'react-webcam'
-import './App.css'
-import { drawHand } from './utils'
+import React, { useRef, useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam from "react-webcam";
+import "./App.css";
+import { drawHand } from "./utils";
+import * as fp from "fingerpose";
+import victory from "./victory.png";
+import thumbs_up from "./thumbs_up.png";
 
-const App = () => {
-  const webcamRef = useRef(null)
-  const canvasRef = useRef(null)
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
 
-  const runHanpose = async () => {
-    const net = await handpose.load()
+  const [imgView, setImgView] = useState(null);
+  const images = { thumbs_up: thumbs_up, victory: victory };
+
+  const runHandpose = async () => {
+    const net = await handpose.load();
     setInterval(() => {
-      detect(net)
-    }, 10)
-  }
+      detect(net);
+    }, 10);
+  };
 
   const detect = async (net) => {
     if (
-      typeof webcamRef.current !== 'undefined' &&
+      typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
       webcamRef.current.video.readyState === 4
     ) {
-      const video = webcamRef.current.video
-      const videoWidth = webcamRef.current.video.videoWidth
-      const videoHeight = webcamRef.current.video.videoHeight
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
 
-      webcamRef.current.video.width = videoWidth
-      webcamRef.current.video.height = videoHeight
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
 
-      canvasRef.current.width = videoWidth
-      canvasRef.current.height = videoHeight
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
 
+      const hand = await net.estimateHands(video);
 
-      const hand = await net.estimateHands(video)
+      if (hand.length > 0) {
+        const GE = new fp.GestureEstimator([
+          fp.Gestures.VictoryGesture,
+          fp.Gestures.ThumbsUpGesture,
+        ]);
+        const gesture = await GE.estimate(hand[0].landmarks, 8.5);
+        console.log(gesture, gesture.poseData);
+        if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
 
-      const ctx = canvasRef.current.getContext('2d')
-      drawHand(hand, ctx)
+          const confidence = gesture.gestures.map(
+            (prediction) => {
+              return prediction.score
+            }
+          );
+          const maxConfidence = confidence.indexOf(
+            Math.max.apply(null, confidence)
+          );
+          setImgView(gesture.gestures[maxConfidence].name);
+        }
+      } else {
+        setImgView('')
+      }
+      
+      const ctx = canvasRef.current.getContext("2d");
+      drawHand(hand, ctx);
     }
-  }
+  };
 
-  useEffect(() => {
-    runHanpose()
-  }, [])
+  useEffect(() => { runHandpose() }, []);
 
   return (
     <div className='App'>
       <header className='App-header'>
         <Webcam ref={webcamRef} className='webcam' />
         <canvas ref={canvasRef} className='canvas' />
+
+        {imgView !== null ? (
+          <img
+            src={images[imgView]}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 400,
+              bottom: 500,
+              right: 0,
+              textAlign: "center",
+              height: 100,
+              zIndex: 999
+            }}
+          />
+        ) : (
+          ""
+        )}
       </header>
     </div>
   )
